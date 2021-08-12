b7da17c17545325c9c6a5d7d002ccb57969638b9
nathaliafcardoso <nathaliaf.cardoso@outlook.com>
2021-08-12 15:50:17 -0300

    Projeto Controle de ponto

---
+++ src/main/java/com/dio/demo/ProjetoControleDePontoGradleApplication.java
@@ -0,0 +1,13 @@
+package com.dio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoControleDePontoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoControleDePontoGradleApplication.class, args);
+	}
+
+}
